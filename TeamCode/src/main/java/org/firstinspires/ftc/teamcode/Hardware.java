13a0ecef9f33b189baeb85f00417fcab9a65ae80
missing <piecenotfound@gmail.com>
2024-11-05 16:56:15 -0600

    Update Hardware

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -5,17 +5,25 @@ import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.units.Angle;
 
 public class Hardware {
-    public DcMotorEx fl, fr, bl, br, horizL, horizR, vertL, vertR;
+    public DcMotorEx fl, fr, bl, br;
+    public DcMotorEx liftL, liftR;
+    public Servo claw;
+    public Servo extendL, extendR;
+    public Servo wristL, wristR;
+    public CRServo intake;
+    public Servo bucketL, bucketR;
     public IMU imu;
 
     public OpMode opMode;
@@ -33,52 +41,73 @@ public class Hardware {
         fr = hardwareMap.get(DcMotorEx.class, "fr");
         bl = hardwareMap.get(DcMotorEx.class, "bl");
         br = hardwareMap.get(DcMotorEx.class, "br");
-        horizL = hardwareMap.get(DcMotorEx.class, "horizontalLeft");
-        horizR = hardwareMap.get(DcMotorEx.class, "horizontalRight");
-        vertL = hardwareMap.get(DcMotorEx.class, "verticalLeft");
-        vertR = hardwareMap.get(DcMotorEx.class, "verticalRight");
 
         fl.setDirection(DcMotorSimple.Direction.REVERSE);
         fr.setDirection(DcMotorSimple.Direction.FORWARD);
         bl.setDirection(DcMotorSimple.Direction.REVERSE);
         br.setDirection(DcMotorSimple.Direction.FORWARD);
-        vertL.setDirection(DcMotorSimple.Direction.REVERSE);
-        vertR.setDirection(DcMotorSimple.Direction.FORWARD);
-        horizL.setDirection(DcMotorSimple.Direction.REVERSE);
-        horizR.setDirection(DcMotorSimple.Direction.FORWARD);
-
-        fl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        fr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        bl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        br.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        vertL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        vertR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        horizL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        horizR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         fl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         fr.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         bl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         br.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        vertL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        vertR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        horizL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        horizR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         fl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         fr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         bl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         br.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        horizL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        horizR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        vertL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        vertR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        liftL = hardwareMap.get(DcMotorEx.class, "liftL");
+        liftR = hardwareMap.get(DcMotorEx.class, "liftR");
+        liftL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        liftR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        liftL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        liftR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        liftL.setDirection(DcMotorSimple.Direction.REVERSE);
+        liftR.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        claw = hardwareMap.get(Servo.class, "claw");
+        extendL = hardwareMap.get(Servo.class, "extendL");
+        extendR = hardwareMap.get(Servo.class, "extendR");
+        wristL = hardwareMap.get(Servo.class, "wristL");
+//        wristR = hardwareMap.get(Servo.class, "wristR");
+        intake = hardwareMap.get(CRServo.class, "intake");
+        bucketL = hardwareMap.get(Servo.class, "bucketL");
+//        bucketR = hardwareMap.get(Servo.class, "bucketR");
+
+        // calibrated on 2024-11-05
+
+        // claw: open 0.86 - closed 0.55,
+        claw.scaleRange(0.55, 0.86);
+        claw.setDirection(Servo.Direction.REVERSE);
+
+        //            in - out
+        // extendL  0.17 - 0.00, port ch5
+        // extendR  0.15 - 0.32, port eh0
+        extendL.scaleRange(0.00, 0.17);
+        extendL.setDirection(Servo.Direction.REVERSE);
+        extendR.scaleRange(0.15, 0.32);
+        extendR.setDirection(Servo.Direction.FORWARD);
+
+        //         down - up
+        // wristL  0.00 - 0.75, port eh3
+        // wristR
+        wristL.scaleRange(0.00, 0.75);
+        wristL.setDirection(Servo.Direction.FORWARD);
+
+        // intake - +1 in, -1 out, port eh5
+        intake.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        //          down - up
+        // bucketL  0.10 - 0.65, port ch3
+        // bucketR
+        bucketL.scaleRange(0.10, 0.65);
+        bucketL.setDirection(Servo.Direction.FORWARD);
 
         imu = hardwareMap.get(IMU.class, "imu");
         imu.initialize(new IMU.Parameters(
-                // TODO: real chub orientation
                 new RevHubOrientationOnRobot(
-                        RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                        RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
                         RevHubOrientationOnRobot.UsbFacingDirection.UP
                 )
         ));
