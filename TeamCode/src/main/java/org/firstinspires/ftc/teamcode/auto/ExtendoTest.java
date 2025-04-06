93591020b13a2d4a602a1b979edede956d610597
missing <piecenotfound@gmail.com>
2025-04-06 17:06:53 -0500

    Add ability to move extendo based on real distances

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/ExtendoTest.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.OpModeWrapper;
+import org.firstinspires.ftc.teamcode.hardware.InWrist;
+import org.firstinspires.ftc.teamcode.hardware.OutShoulder;
+import org.firstinspires.ftc.teamcode.teleop.Gamepads;
+import org.firstinspires.ftc.teamcode.units.Distance;
+
+@TeleOp
+public class ExtendoTest extends OpModeWrapper {
+    public int inches = 0;
+
+    public void setup() {
+        hardware.outShoulder.setPosition(OutShoulder.State.PRE_TRANSFER);
+        hardware.inWrist.setPosition(InWrist.State.DOWN);
+    }
+
+    public void run() {
+        if (gamepads.justPressed(Gamepads.Button.GP1_DPAD_UP)) inches++;
+        if (gamepads.justPressed(Gamepads.Button.GP1_DPAD_DOWN)) inches--;
+        telemetry.addData("inches", inches);
+        hardware.extend.setClawX(Distance.inInches(inches));
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Extendo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Extendo.java
@@ -1,5 +1,10 @@
 package org.firstinspires.ftc.teamcode.hardware;
 
+import androidx.annotation.CheckResult;
+
+import org.firstinspires.ftc.teamcode.macro.Action;
+import org.firstinspires.ftc.teamcode.units.Distance;
+
 public class Extendo extends TimedServo.Pair {
     public static final double IN = 0.0;
     public static final double OUT_GRABBER_UP = 0.74; // horizontal expansion limit
@@ -15,8 +20,27 @@ public class Extendo extends TimedServo.Pair {
                 raw.extendR,
                 1.0, // TODO:
                 0.0,
-                0.05, 0.82,
-                0.95, 0.18
+                0.00, 0.82,
+                1.00, 0.18
         );
+    }
+
+    static final Distance C = Distance.inMM(180);
+    static final Distance K1 = Distance.inMM(377.33166); // 436.767
+    static final double K2 = 1.50773; // 1.13112
+    private double posFromClawX(Distance x) {
+        double x2 = x.sub(C).div(K1);
+        if (x2 < 0) return 0;
+        if (x2 > 1) return 1;
+        return Math.asin(x2) / K2;
+    }
+
+    public void setClawX(Distance x) {
+        this.setPosition(posFromClawX(x));
+    }
+
+    @CheckResult(suggest = "setClawX(Distance)")
+    public Action toClawX(Distance x) {
+        return this.goTo(posFromClawX(x));
     }
 }
